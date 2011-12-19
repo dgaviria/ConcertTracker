34ff43d6e510cab575954c33ccfb0de04b601e78
Daniel Gaviria <ggdaniel@gmail.com>
2011-12-19 11:21:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConcertTracker::Application.config.secret_token = '240bb0b054d39abd942e94f951d2597e4b4ef14d3ef18d0d933ad62c8853a55bc9be2e74180e95cba986db62962b6ab18e024ea02e1c502f88a911d50255115d'
